dc8736856e4b504b1e8b4df43072b93446d9edc2
Volodymyr199606 <your-email@example.com>
2025-05-06 17:11:29 -0700

    "Initial commit of Spring Boot backend project"

---
+++ src/main/java/com/curbside/parking/backend/CurbsideParkingApplication.java
@@ -0,0 +1,13 @@
+package com.curbside.parking.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurbsideParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurbsideParkingApplication.class, args);
+	}
+
+}
