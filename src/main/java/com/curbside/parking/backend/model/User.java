cb5075a8febd9bf5a5f2e730ee733fd6273ad9de
Volodymyr199606 <your-email@example.com>
2025-05-09 10:04:26 -0700

    add User logic

---
+++ src/main/java/com/curbside/parking/backend/model/User.java
@@ -0,0 +1,23 @@
+package com.curbside.parking.backend.model;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+}
