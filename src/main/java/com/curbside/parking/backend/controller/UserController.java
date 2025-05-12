6540bd4dbd628c9e132083aa8507bec2f89cf19e
Volodymyr199606 <your-email@example.com>
2025-05-12 11:05:19 -0700

    implementing login logic

--- src/main/java/com/curbside/parking/backend/controller/UserController.java
+++ src/main/java/com/curbside/parking/backend/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.curbside.parking.backend.controller;
+import com.curbside.parking.backend.dto.LoginRequest;
+import com.curbside.parking.backend.model.User;
 import com.curbside.parking.backend.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +22,15 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            User user = userService.login(loginRequest.getEmail(), loginRequest.getPassword());
+            return ResponseEntity.ok(user);
+        } catch (UsernameNotFoundException | BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+        }
+    }
 }
