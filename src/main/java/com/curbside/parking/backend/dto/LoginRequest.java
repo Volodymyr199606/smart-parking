5f3349a65f645d8bc64de54dd6fc5ae610e4683a
Volodymyr199606 <your-email@example.com>
2025-05-12 15:02:38 -0700

    update LoginRequest

--- src/main/java/com/curbside/parking/backend/dto/LoginRequest.java
+++ src/main/java/com/curbside/parking/backend/dto/LoginRequest.java
@@ -1,4 +1,20 @@
 package com.curbside.parking.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
