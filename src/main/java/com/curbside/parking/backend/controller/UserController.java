f9a9dd86c2a0a762f0f2cbd71fd489a44caa48d0
Volodymyr199606 <your-email@example.com>
2025-05-13 00:10:26 -0700

    implement logic

--- src/main/java/com/curbside/parking/backend/controller/UserController.java
+++ src/main/java/com/curbside/parking/backend/controller/UserController.java
@@ -6,10 +6,9 @@ import com.curbside.parking.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -39,5 +38,17 @@ public class UserController {
         System.out.println("Received user: " + user.getEmail() + ", " + user.getPassword());
         User registeredUser = userService.registerUser(user);
         return ResponseEntity.ok(registeredUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) return ResponseEntity.ok(user);
+        else return ResponseEntity.notFound().build();
     }
 }
