4378a6efab828fd1ce01922286c5ccdacde65b85
Volodymyr199606 <your-email@example.com>
2025-05-15 16:18:23 -0700

    implementing logic

---
+++ src/main/java/com/curbside/parking/backend/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.curbside.parking.backend.impl;
+
+import com.curbside.parking.backend.repository.UserRepository;
+import com.curbside.parking.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
