1ce8ee5493268f0e9bdf315fd800cfdf1f4d5f66
Volodymyr199606 <your-email@example.com>
2025-05-16 00:21:32 -0700

    implement beans

--- src/main/java/com/curbside/parking/backend/impl/UserServiceImpl.java
+++ src/main/java/com/curbside/parking/backend/impl/UserServiceImpl.java
@@ -4,7 +4,11 @@ import com.curbside.parking.backend.model.User;
 import com.curbside.parking.backend.repository.UserRepository;
 import com.curbside.parking.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -29,5 +33,17 @@ public class UserServiceImpl implements UserService {
             e.printStackTrace();
             throw e;
         }
+
+        @Override
+        public List<User> getAllUsers() {
+            return userRepository.findAll();
+        }
+
+        @Override
+        public User getUserById(Long id) {
+            Optional<User> user = userRepository.findById(id);
+            return user.orElse(null);
+        }
+
     }
 }
