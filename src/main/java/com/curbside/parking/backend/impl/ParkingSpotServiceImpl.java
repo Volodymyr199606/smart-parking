853fe9152e4c07169c00e8d7f9c1c17d857f8b0e
Volodymyr199606 <your-email@example.com>
2025-05-19 12:26:45 -0700

    update class

--- src/main/java/com/curbside/parking/backend/impl/ParkingSpotServiceImpl.java
+++ src/main/java/com/curbside/parking/backend/impl/ParkingSpotServiceImpl.java
@@ -1,4 +1,37 @@
 package com.curbside.parking.backend.impl;
 
-public class ParkingSpotServiceImpl {
+import com.curbside.parking.backend.model.ParkingSpot;
+import com.curbside.parking.backend.service.ParkingSpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ParkingSpotServiceImpl implements ParkingSpotService {
+
+    private final ParkingSpotRepository repository;
+
+    @Value("${google.api.key}")
+
+
+    private String apiKey;
+
+    @Autowired
+    private SpotBroadcastService broadcaster;
+
+    @Autowired
+    private SpotBroadcastService broadcaster;
+
+
+    @Autowired
+    public ParkingSpotServiceImpl(ParkingSpotRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<ParkingSpot> getAllSpots() {
+        return repository.findAll();
+    }
 }
