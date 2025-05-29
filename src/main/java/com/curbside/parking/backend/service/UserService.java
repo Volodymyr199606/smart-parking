f435ab9f1502e0bfc9a6325d5a11fe1503bee4e9
Volodymyr199606 <your-email@example.com>
2025-05-29 15:49:07 -0700

    update & implement new logic

--- src/main/java/com/curbside/parking/backend/service/UserService.java
+++ src/main/java/com/curbside/parking/backend/service/UserService.java
@@ -1,18 +1,9 @@
 package com.curbside.parking.backend.service;
 
-
+import com.curbside.parking.backend.dto.UpdateProfileRequest;
 import com.curbside.parking.backend.model.User;
 
-import java.util.List;
-
 public interface UserService {
-
-    User registerUser(User user);
-
-    List<User> getAllUsers();
-
-    User getUserById(Long id);
-
-    User login(String email, String rawPassword);
-
+    User getCurrentUser(String email);
+    User updateUserProfile(String email, UpdateProfileRequest request);
 }
