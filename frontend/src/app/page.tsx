f1ebe01187ab79c1e89d6d11895099a6f31f29c4
Volodymyr199606 <your-email@example.com>
2025-06-08 14:35:34 -0700

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,2 @@
 // src/app/page.tsx
-export default function Home() {
-    return (
-        <div>
-            <h1>Welcome to Smart Parking</h1>
-            <p><a href="/home">Go to homepage</a></p>
-        </div>
-    );
-}
+export { default } from './home/page'
